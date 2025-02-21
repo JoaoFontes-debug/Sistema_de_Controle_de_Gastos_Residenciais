c22b702f389450608f8030ccd1c6af40604d7378
joao fontes <j.henry.kings@gmail.com>
2025-02-20 23:21:15 -0300

    Finished documentation

--- src/main/java/com/teste/gestaofinanceira/GestaoFinanceiraApplication.java
+++ src/main/java/com/teste/gestaofinanceira/GestaoFinanceiraApplication.java
@@ -5,7 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GestaoFinanceiraApplication {
-
+	/**
+	 * OBS: utilize o link no seu navegador para testa a API
+	 *
+	 * http://localhost:8080/swagger-ui/index.html#/
+	 *Para testar a API é simples basta executar o programa
+	 * e abrir o link(http://localhost:8080/swagger-ui/index.html#/) do swagger
+	 * no seu navegador, depois é só clicar em "try out" e depois em "execute"
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(GestaoFinanceiraApplication.class, args);
 
